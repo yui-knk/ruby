0e92c469787eafe77889c7508aa286c1bdd41f97
yui-knk <spiketeika@gmail.com>
2024-01-12 10:46:17 +0900

    [Feature #20257] Rearchitect Ripper
    Introduce another semantic value stack for Ripper so that
    Ripper can manage both Node and Ruby Object separately.
    This rearchitectutre of Ripper solves these issues.
    Therefore adding test cases for them.
    
    * [Bug 10436] https://bugs.ruby-lang.org/issues/10436
    * [Bug 18988] https://bugs.ruby-lang.org/issues/18988
    * [Bug 20055] https://bugs.ruby-lang.org/issues/20055
    
    Checked the differences of `Ripper.sexp` for files under `/test/ruby`
    are only on test_pattern_matching.rb.
    The differences comes from the differences between
    `new_hash_pattern_tail` functions between parser and Ripper.
    Ripper `new_hash_pattern_tail` didn’t call `assignable` then
    `kw_rest_arg` wasn’t marked as local variable.
    This is also fixed by this commit.
    
    ```
    --- a/./tmp/before/test_pattern_matching.rb
    +++ b/./tmp/after/test_pattern_matching.rb
    @@ -3607,7 +3607,7 @@
                      [:in,
                       [:hshptn, nil, [], [:var_field, [:@ident, “a”, [984, 13]]]],
                       [[:binary,
    -                    [:vcall, [:@ident, “a”, [985, 10]]],
    +                    [:var_ref, [:@ident, “a”, [985, 10]]],
                         :==,
                         [:hash, nil]]],
                       nil]]],
    @@ -3662,7 +3662,7 @@
                      [:in,
                       [:hshptn, nil, [], [:var_field, [:@ident, “a”, [993, 13]]]],
                       [[:binary,
    -                    [:vcall, [:@ident, “a”, [994, 10]]],
    +                    [:var_ref, [:@ident, “a”, [994, 10]]],
                         :==,
                         [:hash,
                          [:assoclist_from_args,
    @@ -3813,7 +3813,7 @@
                        [:command,
                         [:@ident, “raise”, [1022, 10]],
                         [:args_add_block,
    -                     [[:vcall, [:@ident, “b”, [1022, 16]]]],
    +                     [[:var_ref, [:@ident, “b”, [1022, 16]]]],
                          false]]],
                       [:else, [[:var_ref, [:@kw, “true”, [1024, 10]]]]]]]],
                    nil,
    @@ -3876,7 +3876,7 @@
                          [:@int, “0”, [1033, 15]]],
                         :“&&“,
                         [:binary,
    -                     [:vcall, [:@ident, “b”, [1033, 20]]],
    +                     [:var_ref, [:@ident, “b”, [1033, 20]]],
                          :==,
                          [:hash, nil]]]],
                       nil]]],
    @@ -3946,7 +3946,7 @@
                          [:@int, “0”, [1042, 15]]],
                         :“&&“,
                         [:binary,
    -                     [:vcall, [:@ident, “b”, [1042, 20]]],
    +                     [:var_ref, [:@ident, “b”, [1042, 20]]],
                          :==,
                          [:hash,
                           [:assoclist_from_args,
    @@ -5206,7 +5206,7 @@
                          [[:assoc_new,
                            [:@label, “c:“, [1352, 22]],
                            [:@int, “0”, [1352, 25]]]]]],
    -                   [:vcall, [:@ident, “r”, [1352, 29]]]],
    +                   [:var_ref, [:@ident, “r”, [1352, 29]]]],
                       false]]],
                    [:binary,
                     [:call,
    @@ -5299,7 +5299,7 @@
                           [:assoc_new,
                            [:@label, “c:“, [1367, 34]],
                            [:@int, “0”, [1367, 37]]]]]],
    -                   [:vcall, [:@ident, “r”, [1367, 41]]]],
    +                   [:var_ref, [:@ident, “r”, [1367, 41]]]],
                       false]]],
                    [:binary,
                     [:call,
    @@ -5931,7 +5931,7 @@
                  [:in,
                   [:hshptn, nil, [], [:var_field, [:@ident, “r”, [1533, 11]]]],
                   [[:binary,
    -                [:vcall, [:@ident, “r”, [1534, 8]]],
    +                [:var_ref, [:@ident, “r”, [1534, 8]]],
                     :==,
                     [:hash,
                      [:assoclist_from_args,
    ```

--- node.c
+++ node.c
@@ -69,7 +69,6 @@ rb_node_buffer_new(void)
     init_node_buffer_list(&nb->unmarkable, (node_buffer_elem_t*)&nb[1], ruby_xmalloc);
     init_node_buffer_list(&nb->markable, (node_buffer_elem_t*)((size_t)nb->unmarkable.head + bucket_size), ruby_xmalloc);
     nb->local_tables = 0;
-    nb->mark_hash = Qnil;
     nb->tokens = Qnil;
 #ifdef UNIVERSAL_PARSER
     nb->config = config;
@@ -415,7 +414,6 @@ void
 rb_ast_mark(rb_ast_t *ast)
 {
     if (ast->node_buffer) {
-        rb_gc_mark(ast->node_buffer->mark_hash);
         rb_gc_mark(ast->node_buffer->tokens);
         node_buffer_t *nb = ast->node_buffer;
         iterate_node_values(ast, &nb->markable, mark_ast_value, NULL);
@@ -462,22 +460,6 @@ void
 rb_ast_dispose(rb_ast_t *ast)
 {
     rb_ast_free(ast);
-}
-
-void
-rb_ast_add_mark_object(rb_ast_t *ast, VALUE obj)
-{
-    if (NIL_P(ast->node_buffer->mark_hash)) {
-        RB_OBJ_WRITE(ast, &ast->node_buffer->mark_hash, rb_ident_hash_new());
-    }
-    rb_hash_aset(ast->node_buffer->mark_hash, obj, Qtrue);
-}
-
-void
-rb_ast_delete_mark_object(rb_ast_t *ast, VALUE obj)
-{
-    if (NIL_P(ast->node_buffer->mark_hash)) return;
-    rb_hash_delete(ast->node_buffer->mark_hash, obj);
 }
 
 VALUE
